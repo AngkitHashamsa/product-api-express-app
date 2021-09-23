4395e4efbeeeae8a27564c7e2a9cc75523c1da53
Angkit <angkithashamsa21@gmail.com>
2021-09-23 15:42:40 +0530

    correct PORT

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 require('express-async-errors')
 const express = require('express')
 const app = express()
-const port = process.env.Port || 5000
+const port = process.env.PORT || 5000
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 const router = require('./routes/products')
